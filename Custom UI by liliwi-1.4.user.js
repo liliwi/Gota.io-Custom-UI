31d7ad2e3ae68877db6bc06f0cf92bf6671063e0
liliwi <linus.lindahl08@gmail.com>
2025-11-02 21:00:33 +0100

    Update Custom UI by liliwi-1.41.user.js
    fixed the update url and download url

--- Custom UI by liliwi-1.4.user.js
+++ Custom UI by liliwi-1.4.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Custom UI by liliwi
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.41
 // @description  just a ui
 // @author       liliwi
 // @discord      liliwi
@@ -9,8 +9,9 @@
 // @match        https://gota.io/web/*
 // @match        https://play.gota.io/*
 // @grant        GM_addStyle
-// @updateURL    https://github.com/liliwi/Gota.io-Custom-UI/raw/refs/heads/main/Custom%20UI%20by%20liliwi-1.4.user.js
-// @downloadURL  https://github.com/liliwi/Gota.io-Custom-UI/raw/refs/heads/main/Custom%20UI%20by%20liliwi-1.4.user.js
+// @updateURL    https://raw.githubusercontent.com/liliwi/Gota.io-Custom-UI/main/Custom%20UI%20by%20liliwi-1.4.user.js
+// @downloadURL  https://raw.githubusercontent.com/liliwi/Gota.io-Custom-UI/main/Custom%20UI%20by%20liliwi-1.4.user.js
+
 // ==/UserScript==
 
 (function() {    'use strict';
